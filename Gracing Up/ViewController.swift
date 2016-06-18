8adb47aeb23b37cef7ebfbc21ddeca640dfaefca
Sarah Melvin <snmelvin@email.wm.edu>
2016-06-18 12:40:56 -0400

    Initial Commit

---
+++ Gracing Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gracing Up
+//
+//  Created by Sarah Melvin on 6/18/16.
+//  Copyright © 2016 ERP Resources. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
