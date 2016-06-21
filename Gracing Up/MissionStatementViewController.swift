9e78140f3145792574f074a4a3ca05ec24c26e12
Jake Shor <jakeshor@jakes-mac.local>
2016-06-20 21:58:54 -0400

    added new basic pages and connected the pages using the buttons on them

---
+++ Gracing Up/MissionStatementViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MissionStatementViewController.swift
+//  Gracing Up
+//
+//  Created by Jake Shor on 6/20/16.
+//  Copyright © 2016 ERP Resources. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MissionStatementViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
